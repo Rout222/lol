78f548e68dd54f512f87b2bfa8a98e6bd8205094
Rout222 <routmagno@gmail.com>
2019-01-18 11:44:47 -0200

    bkp

--- prediction.py
+++ prediction.py
@@ -11,14 +11,17 @@ class predict(threading.Thread):
 		self.step = step
 		self.bd = mysql.connector.connect(passwd="",db="lol_kaggle", user="root")
 		self.cursor = self.bd.cursor()
+		self.i = 0
 
 	def	cleanup_stop_thread(self):
 		self.bd.close()
 	
 	def salva(self, dados, matchid):
+		self.i = self.i + 1
 		self.cursor.execute("INSERT INTO `predict`(`p1ep2`, `p1ep3`, `p1ep4`, `p1ep5`, `p1vp6`, `p1vp7`, `p1vp8`, `p1vp9`, `p1vp10`, `p2ep3`, `p2ep4`, `p2ep5`, `p2vp6`, `p2vp7`, `p2vp8`, `p2vp9`, `p2vp10`, `p3ep4`, `p3ep5`, `p3vp6`, `p3vp7`, `p3vp8`, `p3vp9`, `p3vp10`, `p4ep5`, `p4vp6`, `p4vp7`, `p4vp8`, `p4vp9`, `p4vp10`, `p5vp6`, `p5vp7`, `p5vp8`, `p5vp9`, `p5vp10`, `p6ep7`, `p6ep8`, `p6ep9`, `p6ep10`, `p7ep8`, `p7ep9`, `p7ep10`, `p8ep9`, `p8ep10`, `p9ep10`, `timevencedor`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", dados)
 		self.cursor.execute("UPDATE `players` SET `processado`= 1 WHERE `match_id` = {}".format(matchid))
-		self.bd.commit()
+		if self.i % 20:
+			self.bd.commit()
 
 	def run(self):
 		while True:
@@ -41,10 +44,14 @@ class predict(threading.Thread):
 				dados.append(int(herois[0][1] == 1))
 				self.salva(dados, matchid)
 
-db=mysql.connector.connect(passwd="",db="lol_kaggle", user="root")
-c=db.cursor()
-c.execute("SELECT DISTINCT(`match_id`) FROM `players` WHERE processado = 0")
-quantidade = c.fetchall()
+try:
+	db=mysql.connector.connect(passwd="",db="lol_kaggle", user="root")
+	c=db.cursor()
+	c.execute("SELECT DISTINCT(`match_id`) FROM `players` WHERE processado = 0")
+	quantidade = c.fetchall()
+except Exception as e:
+	print("MYSQL ERROR, saindo...")
+	exit()
 
 start = time.clock()
 threadG = []
